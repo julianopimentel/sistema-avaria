aa60f94500ff33e3fedb4f83f8a2ac52a0d80d04
Juliano Pimentel <juliano_pimentel@aol.com>
2018-11-18 02:35:12 -0400

    Limpeza

--- colegio.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `funcionario` (
-  `idFuncionario` INT NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) NULL,
-  `email` varchar(150) NULL,
-  `senha` varchar(45) NULL,
-  `data_cadastro` datetime NULL,
-  PRIMARY KEY (`idFuncionario`)
-) ENGINE=InnoDB;
