95bce851df06bd743cd36e9c3eca04ee9b89dbc1
Juliano Pimentel <juliano_pimentel@aol.com>
2018-11-18 03:09:35 -0400

    renomear

--- form-login.php
+++ form-login.php
@@ -1,30 +1,37 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
- 
-        <title>Login | Sistema de Login ULTIMATE PHP</title>
-    </head>
- 
-    <body>
-         
-        <h1> Login</h1>
- 
-        <form action="login.php" method="post">
-            <label for="email">Email: </label>
-            <br>
-            <input type="text" name="email" id="email">
- 
-            <br><br>
- 
-            <label for="password">Senha: </label>
-            <br>
-            <input type="password" name="password" id="password">
- 
-            <br><br>
- 
-            <input type="submit" value="Entrar">
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Sistema de login</title>
+<link href="css/estilo-index.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+</head>
+
+<body>
+        <div id="areaLogin">
+
+         <form action="login.php" method="post">
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon">@</span>
+                    <input type="text" name="email" class="form-control" id="email" placeholder="E-mail" required="required" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                    <input type="password" name="senha" class="form-control" id="password" placeholder="Senha">
+                </div> 
+            </div>
+            <div class="form-group">
+                <button type="submit" name="btLogar" value="Entrar" class="btn btn-primary btn-block">Logar</button> 
+            </div>
         </form>
- 
-    </body>
+       <?php if(isset($_GET["login"]) == "error"){ ?>
+        <div class="alert alert-danger alert-block alert-aling" role="alert">Ops! E-mail ou Senha estão errado</div>
+        <?php } ?>
+    </div>
+</body>
 </html>
