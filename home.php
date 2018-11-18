4ea672d299fbe1ed99fc4718923b2a1ba49bd2cb
Juliano Pimentel <juliano_pimentel@aol.com>
2018-11-18 07:34:51 -0400

    Update home.php

--- home.php
+++ home.php
@@ -70,13 +70,6 @@
        <div class="col">
           <div class="tab-content" id="nav-tabContent">
            	
-           <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Dashboard</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
             <!-- /.row -->
             <div class="row">
                 <div class="col-lg-3 col-md-6">
