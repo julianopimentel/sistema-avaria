f73746f0f02376905571f0ee5d7c37149427b3d9
Juliano Pimentel <juliano_pimentel@aol.com>
2018-11-18 02:42:42 -0400

    a

---
+++ init.php
@@ -0,0 +1,15 @@
+<?php
+ 
+// constantes com as credenciais de acesso ao banco MySQL
+define('DB_HOST', 'localhost');
+define('DB_USER', 'u617793640_acess');
+define('DB_PASS', 'fHtjYbni84Xs');
+define('DB_NAME', 'u617793640_cocil');
+
+ 
+// habilita todas as exibições de erros
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+ 
+// inclui o arquivo de funçõees
+require_once 'functions.php';
