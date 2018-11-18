09eb7fd6113b1c38c4fbe10d77c55fe8ed4b5b4a
Juliano Pimentel <juliano_pimentel@aol.com>
2018-11-18 04:38:42 -0400

    correcao login

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
-<link rel="icon" href="../../../../favicon.ico">
+<link rel="icon" href="icon/favicon.ico">
 <link href="css/estilo-index.css" rel="stylesheet" type="text/css" media="all">
 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
 <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" media="all">
@@ -15,7 +15,7 @@
   <body class="text-center">
 
     <form class="form-signin" action="login.php" method="post">
-      <img class="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <img class="mb-4" src="icon/logo.svg" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" name="email" class="form-control" id="email" placeholder="E-mail" required="required" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
